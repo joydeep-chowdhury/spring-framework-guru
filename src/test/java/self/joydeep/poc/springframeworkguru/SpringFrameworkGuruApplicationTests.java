800a871a91286a49bdefcf396391d672d3e16dcf
Joydeep <joydeep.chowdhury@oracle.com>
2021-07-18 19:22:02 +0530

    First commit

---
+++ src/test/java/self/joydeep/poc/springframeworkguru/SpringFrameworkGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package self.joydeep.poc.springframeworkguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
